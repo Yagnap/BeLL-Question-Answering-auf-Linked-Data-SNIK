be6dc9b6a34e4d4cb03d4f5378a7c11565281f08
H. R. B <51321855+Yagnap@users.noreply.github.com>
2022-06-29 17:08:42 +0200

    Update AutomatischeFragen.js

--- Scripts/AutomatischeFragen.js
+++ Scripts/AutomatischeFragen.js
@@ -3,19 +3,35 @@
   * Step 1: Login to gain Token
   * (Loop changing type of questions asked - textbook, generated, both, none)
   * (Loop incrementing training question count by 100)
-  * Step 2: Create Dataset -> Upload Questions
-  * Step 3: Configure Dataset
-
-  * Step 4: Reset Dataset ML Model
-  * Step 5: Train
-  * Step 6: Evaluate
+  * Step 2: Reset Dataset ML Model
+  * Step 3: Train
+  * Step 4: Evaluate
   *
   *
   * API Reference: https://qanswer-core1.univ-st-etienne.fr/swagger-ui.html#/
 */
 
 var token;
-var bb;
+const dataset = "SNIK_BB";
+
+var noTraining;
+var genOnly;
+var bookOnly;
+var both;
+
+const trainbook = [{
+
+}]; // TODO Trainingsfragen aus BB
+const testbook = [{
+
+}]; // TODO Testfragen aus BB
+
+const traingen = [{
+
+}]; // TODO Trainingsfragen automatisch generiert
+const testgen = [{
+
+}]; // TODO Testfragen automatisch generiert
 
 async function login() {
   let settings = {
@@ -27,7 +43,7 @@ async function login() {
       "Content-Type": "application/json",
     },
     processData: false,
-    data: '{"usernameOrEmail": "' + QANSWER_CREDENTIALS.user + '", "password":"' + QANSWER_CREDENTIALS.password + '"}',
+    data: '{"usernameOrEmail": "' + QANSWER_CREDENTIALS.user + '", "password":"' + QANSWER_CREDENTIALS.password + '"}'
   };
 
   $.ajax(settings).done(function (response) {
@@ -36,100 +52,161 @@ async function login() {
   });
 }
 
-function loadSnikData() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/bb.nt");
-  xhr.responseType = "blob";//force the HTTP response, response-type header to be blob
-  xhr.onload = function()
-  {
-      bb = xhr.response;//xhr.response is now a blob object
-  }
-  xhr.send();
-}
-
-async function createDataset(accessToken, dataset) {
-
-  const initdata = {
-    "dataset": dataset,
-    "file":
-  };
-
+async function resetModel() {
   let settings = {
     async: true,
     crossDomain: true,
-    url: "http://qanswer-core1.univ-st-etienne.fr/api/dataset/upload",
+    url: "​/api​/dataset​/set_default_model",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     processData: false,
-    data: JSON.stringify(initdata)
-  };
-}
-
-async function configureDataset(accessToken, dataset) {
-
-  const config = {
-    "autocompleteConfig": {
-      "dataSuggestion": false,
-      "graphSuggestion": false,
-      "questionSuggestion": true
-    },
-    "dataset": dataset,
-    "languages": ["en"],
-    "numberTriples": 2,
-    "propertyMappings": [
-      "updatesPhases": {
-        "lexicalization": "phases",
-        "uri": "http://www.snik.eu/ontology/meta/updates"
-      }
-    ],
-    "stopWords": {
-      "additionalProp": [
-        "a", "about", "all", "an", "and", "are", "as", "at", "be", "by", "can", "did", "do",
-        "does", "from", "give", "goes", "had", "has", "have", "here", "how", "in", "into",
-        "is", "its", "list", "many", "most", "my", "no", "of", "on", "or", "s", "show", "some",
-        "something", "such", "tell", "the", "their", "these", "they", "this", "to", "using",
-        "was", "were", "what", "which", "will", "with", "yes"
-      ]
-    },
-    "uiMappings": {
-      "description": [
-        "http://www.w3.org/2000/01/rdf-schema#description",
-        "http://schema.org/description",
-        "http://www.w3.org/2004/02/skos/core#definition"
-      ],
-      "label": [
-        "http://www.w3.org/2000/01/rdf-schema#label",
-        "http://www.w3.org/2004/02/skos/core#altLabel"
-      ]
-    }
-  };
-  let settings = {
-    async: true,
-    crossDomain: true,
-    url: "http://qanswer-core1.univ-st-etienne.fr/api/dataset/config",
-    method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
-    processData: false,
-    data: JSON.stringify(config)
+    data: [dataset]
   };
+
   $.ajax(settings).done(function (response) {
-    console.log("Configuration finished for " + dataset ".");
-  });
+    token = response.accessToken;
+    console.log("Reset model.");
+  }); 
+}
+
+async function resetQAPairs() {
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "​/api/feedback/remove-all-questions",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    processData: false,
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+    data: [dataset]
+  };
+
+  $.ajax(settings).done(function (response) {
+    token = response.accessToken;
+    console.log("Reset model.");
+  }); 
+}
+
+async function uploadQuestions(questionAnswerPairsJSON) {
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "​/api/feedback/admin/upload",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    processData: false,
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+    data: JSON.stringify(questionAnswerPairsJSON)
+  };
+
+  $.ajax(settings).done(function (response) {
+    token = response.accessToken;
+    console.log("Uploaded question-answer-pairs.");
+  }); 
+}
+
+async function train() {
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "​/api/feedback/train",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    processData: false,
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+    data: [dataset]
+  };
+
+  $.ajax(settings).done(function (response) {
+    token = response.accessToken;
+    console.log("Trained.");
+  }); 
+}
+
+// TODO: AUSWERTUNG; IMMER ALLE FRAGEN ABFRAGEN, ABER GETRENNT RECHNEN
+async function evaluate() {
+
 }
 
 async function main() {
-  var dataset = "SNIK_BB_TEST";
   await login();
-  loadSnikData();
+  // no training
+  await resetModel();
+  await resetQAPairs();
+  noTraining = await evaluate();
 
-  await createDataset(token, dataset);
-  await configureDataset(token, dataset);
+  // only auto-generated questionsk
+  genOnly = array();
+  for(num = 100; num <= traingen.length; num+=100) {
+    if(num > traingen.length) {
+      num = traingen.length;
+    }
 
-  // TODO: Loops
-  // TODO: Dataset name in Format SNIK_BB_TYPE_NUM
+    console.group("GENERATED - " + num);
+    // Setup
+    await resetModel();
+    await resetQAPairs();
+
+    // Training
+    let questions = traingen.slice(0, num);
+    await uploadQuestions(questions);
+
+    let res = await evaluate();
+    gen.array_push(res);
+    
+    console.groupEnd();
+  }
+
+  // only textbook questions for training
+  console.group("TEXTBOOK ONLY");
+  await resetModel();
+  await resetQAPairs();
+  
+  await uploadQuestions(trainbook);
+  await train();
+
+  bookOnly = await evaluate();
+  
+  console.groupEnd();
+
+  // training with both; only incrementing auto-generated count though
+  both = array();
+  for(num = 100; num <= traingen.length; num+=100) {
+    if(num > traingen.length) {
+      num = traingen.length;
+    }
+    // configure
+    console.group("BOTH - " + num);
+    await resetModel();
+    await resetQAPairs();
+
+    // Upload Questions & Train
+    let qapairs = traingen.slice(0, num);
+    qapairs = qapairs.concat(trainbook);
+    
+    await uploadQuestions(qapairs);
+    await train();
+
+    let res = await evaluate();
+    both.push(res);
+
+    console.groupEnd();
+  }
 }
-// VIELLEICHT EINFACH GANZ ÄNDERN: SCHON ERSTELLTEN & KONFIGURIERTEN DATENSATZ VERWENDEN & DANN /api/dataset/set_default_model
