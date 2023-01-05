77884ec07909e86bc1f7c7baac30e2228a3bfb2a
H. R. B <51321855+Yagnap@users.noreply.github.com>
2023-01-05 10:12:35 +0100

    added output for test and training group of questions

--- Scripts/QAnswer-Trainer/fragen-einteilung.js
+++ Scripts/QAnswer-Trainer/fragen-einteilung.js
@@ -1,9 +1,26 @@
+function retreivePairs() {
+    // TODO: Comments
+    // TODO: import JSON file with questions from the SPARQL-queries
+    // TODO: clean JSON data into 2D-Array with individual subarrays containing one QA-Pair
+}
+
 function sort(qaPairs) {
     qaPairs = shuffle(qaPairs);
 
     let halfLength = Math.ceil(qaPairs.length / 2);
     let trainingPairs = qaPairs.slice(0, halfLength);
     let testPairs = qaPairs.slice(halfLength);
+
+    console.groupCollapsed("Training pairs");
+    console.log(JSON.stringify(trainingPairs));
+    console.groupEnd();
+    
+    console.groupCollapsed("Testing pairs");
+    console.log(JSON.stringify(testPairs));
+    console.groupEnd();
+
+    // TODO: Better output into HTML page with copy button
+    
 }
 
 function shuffle(array) {
